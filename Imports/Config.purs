b19f5ad6bf8b26b9346c818446af29dde49f311a
Christian Fischer <christian@chfi.se>
2017-12-07 14:31:26 +0100

    config.org type-checks

---
+++ Imports/Config.purs
@@ -0,0 +1,10 @@
+module Config where
+
+import Prelude
+
+import Data.Maybe (Maybe)
+import Data.StrMap (StrMap)
+import Genetics.Browser.Biodalliance (BrowserConstructor, RenderWrapper)
+import Genetics.Browser.Biodalliance.Config (RendererInfo)
+import Genetics.Browser.Config.Track (TracksMap)
+import Genetics.Browser.Events.TrackSource (SourceConfig)
